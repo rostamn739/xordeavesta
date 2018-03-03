1e1859035030ffbe1404e3b372e12cf14b17e32b
Rostam <rostamn739@gmail.com>
2018-03-03 12:37:02 +0300

    initial commit

---
+++ app/build/gen/org/blagoverie/xorde/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.blagoverie.xorde;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = false;
+}
---
+++ app/src/main/java/org/blagoverie/xorde/MainActivity.java
@@ -0,0 +1,15 @@
+package org.blagoverie.xorde;
+
+import android.os.*;
+import org.apache.cordova.*;
+
+public class MainActivity extends CordovaActivity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Set by <content src="index.html" /> in config.xml
+        loadUrl(launchUrl);
+    }
+}
